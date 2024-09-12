00de47180f067c61b23e66c7fd348c2029e26ce8
t1ffanyh <Tiffanyho0903@gmail.com>
2024-09-12 13:05:59 -0400

    move lemon

--- Lemon.java
+++ Lemon.java
@@ -5,6 +5,26 @@ public class Lemon extends Actor
     public void act()
     {
         // Add your action code here.
+        move(-10);
+        
+        if(getX() <=0)
+        {
+            resetLemon();
+        }
+    }
+    
+    public void resetLemon()
+    {
+        int num = Greenfoot.getRandomNumber(2);
+        if(num == 0)
+        {
+            setLocation(600, 100);
+        }
+        else
+        {
+            setLocation(600, 300);
+        }
+        
         
     }
 }
